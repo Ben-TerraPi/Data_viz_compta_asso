592f007bf7d679a545b6797bfe0ca842259645d7
Benoît Dourdet <benoit.dourdet@gmail.com>
2026-09-15 15:15:42 +0200

    [feat] ADD config.toml

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,22 @@ import streamlit as st
 
 def main():
 
-    
+#>>>>>>>>>>>>>>>>>>>>> Streamlit page
+    st.set_page_config(
+        page_title="LVR compta",
+        page_icon="🧾",
+        layout="wide",
+        initial_sidebar_state="expanded",
+    )
+
+    st.title('La Vilaine Compta ')
+
+    st.header("Outil de Data Viz pour les comptes de l'association")
+
+    st.markdown("""
+    blablabla          
+    """)
+
 
 if __name__ == "__main__":
     main()
